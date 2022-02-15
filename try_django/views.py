d9b57ad05efd661ce8d99ee800eed71cb59be52b
TimzOwen <ktimothy.com>
2022-02-15 21:49:05 +0300

    django pages

--- try_django/views.py
+++ try_django/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
+import random 
 
-HTML_STRING = """
-<h1> This is DJango </h1>
-<h1> Welcome to the world of django programming Timz</h1>
-"""
+
 
 def home_view(request):
 
     """
     takes in a request and returns a response
     """
+    HTML_STRING = """
+    <h1> This is DJango </h1>
+    <h1> Welcome to the world of django programming Timz</h1>
+    """
 
-    return HttpResponse(HTML_STRING)
+    name = "Timz"
+    win = random.randint(10,1000)
+
+    deatils = f"""<h2> "Hello {name} you just win {win}" </h2>"""
+
+
+
+    return HttpResponse(deatils)
